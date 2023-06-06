9c1419bcf061f845ac4c06df43d4698e7624d96d
Laura Gutierrez Funderburk <lgutierrwr@gmail.com>
2023-06-06 13:21:24 -0700

    add docs on usage

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# automate-tech-post
+# Set up
+
+Create a virtual environment
+
+```
+conda create --name postenv python==3.10
+```
+
+Activate
+
+```
+conda activate postenv
+```
+
+Clone repo and install dependencies
+
+```
+git clone https://github.com/lfunderburk/automate-tech-post.git
+cd automate-tech-post/
+pip install -r requirements.txt
+```
+
+Create a `.env` file where you can store your OpenAI API key. The set your key within the `.env` file as follows:
+
+```
+OPENAI_API_KEY = <your-keyy>
+```
+
+## Executing LangChain pipeline
+
+You can execute the pipeline as follows:
+
+```
+python llm-automation/blog_to_post.py
+```
--- llm-automation/blog_to_post.py
+++ llm-automation/blog_to_post.py
@@ -1,117 +1,11 @@
 from langchain.agents import initialize_agent
-from langchain.chat_models import ChatOpenAI
-from langchain.agents import tool
-import requests
-from bs4 import BeautifulSoup
 import json
 from dotenv import load_dotenv
 import openai
+from langchain.chat_models import ChatOpenAI
 import os
 from pathlib import Path
-import pandas as pd
-from utils import Prompter
-from datasets import Dataset
-
-def save_data_to_hugging_face(master_dic, hf_username, dataset_name):
-    """
-    Saves data to Hugging Face
-
-    Parameters
-    ----------
-    master_dic : list
-        A list of dictionaries containing the id, summary and social media post
-    hf_username : str
-        The username of the Hugging Face account
-    dataset_name : str
-        The name of the dataset to be created in Hugging Face
-
-    Returns
-    -------
-    None
-    """
-    id = [item['id'] for item in master_dic]
-    summary =  [item['summary'] for item in master_dic]
-    sm_post = [item['social_media_post'] for item in master_dic]
-
-    df = pd.DataFrame({'id': id, 'summary': summary, 'social_media_post': sm_post})
-
-    # Push to Hugging Face
-    hf_dataset = Dataset.from_pandas(df)
-    
-
-    hf_dataset.push_to_hub(f"{hf_username}/{dataset_name}")
-
-@tool
-def extract_data_from_page(url):
-
-    """
-    Extracts data from a webpage
-    
-    Parameters
-    ----------
-    url : str
-        The url of the webpage to be scraped
-        
-    Returns
-    -------
-    final : list
-        A list of the content of the webpage"""
-
-    # Send a GET request to the webpage
-    response = requests.get(url)
-
-    # Parse the webpage's content
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Find and keep the order of h1, h2, h3, p tags
-    content = []
-    # Look for the tags of interest in order of appearance
-    for tag in soup.find_all(['h1', 'h2', 'h3', 'p', 'pre', 'div']):
-        # Ignore spans with class "caption-text"
-        if tag.name == 'div' and 'sidebar-primary-item' in tag.get('class', []):
-            continue
-
-        # Check if it's the div with class 'cell docutils container'
-        if tag.name == 'div' and 'cell docutils container' in tag.get('class', []):
-            content.append(f"{tag.get_text(strip=True)}")
-        # Check if it's 'pre' tag within the 'div' with class 'cell docutils container'
-        elif tag.name == 'pre' and tag.find_parent('div', {'class': 'cell docutils container'}):
-            content.append(f"{tag.get_text(strip=True)}")
-        elif tag.name in ['h1', 'h2', 'h3', 'p']:
-            content.append(f"{tag.get_text(strip=True)}")
-
-    # Join all the elements together
-    text = "\n".join(content)
-
-    # remove the first 9 lines and the last 6 lines
-    final = "\n".join(text.split("\n")[9:-6])
-
-    return final
-
-@tool
-def generate_post(summary):
-    """
-    Generates a social media post from a summary
-    
-    Parameters
-    ----------
-    summary : str
-        The summary of the content of a webpage
-
-    Returns
-    -------
-    post : str
-        The social media post inviting users to read the content of the webpage
-    """
-
-    prompter = Prompter(api_key=openai.api_key, gpt_model="gpt-4", temperature=1)
-
-    post = prompter.social_media_wizard(summary)
-
-    return post
-
-
-
+from utils import extract_data_from_page, generate_post, save_data_to_hugging_face
 
 if __name__=="__main__":
 
